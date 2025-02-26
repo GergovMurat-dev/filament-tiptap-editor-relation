10236eea869f20b21a6d31a9927a6920931f301a
Murat <muratgergov0820@gmail.com>
2025-02-26 10:47:04 +0300

    Test получение данных в консоли

--- resources/js/plugin.js
+++ resources/js/plugin.js
@@ -340,6 +340,8 @@ export default function tiptap({
 
             this.initEditor(this.state);
 
+            console.log(this.$el.querySelectorAll('relation'));
+
             let sortableEl = this.$el.parentElement.closest("[x-sortable]");
             if (sortableEl) {
                 window.Sortable.utils.on(sortableEl, "start", () => {
